dd40b45f3c90ed5281698fee92c4250a1d774219
Bas Conrads <11506433+BastogneBas@users.noreply.github.com>
2021-11-24 12:46:15 +0100

    Create navigation base

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -16,13 +16,53 @@ using System.Windows.Shapes;
 namespace View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// The main window of the project
     /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Occurs when a navigation item is clicked to navigate to its page
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The event data.</param>
+        private void MainWindowNavigationItem_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindowNavigationItem mwniSender = (MainWindowNavigationItem)sender;
+            tbContentTitle.Text = (string)mwniSender.Content;
+            frContent.Navigate(mwniSender.Page);
+        }
+
+        /// <summary>
+        /// Occurs when the back/forward buttons are clicked
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The event data.</param>
+        private void NavigationNextPreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((Button)sender == btPreviousButton)
+            {
+                frContent.GoBack();
+            }
+            else if ((Button)sender == btNextButton)
+            {
+                frContent.GoForward();
+            }
+        }
+
+        /// <summary>
+        /// Occurs when navigation happened in the frame
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The event data.</param>
+        private void frContent_Navigated(object sender, NavigationEventArgs e)
+        {
+            btPreviousButton.IsEnabled = ((Frame)sender).CanGoBack;
+            btNextButton.IsEnabled = ((Frame)sender).CanGoForward;
         }
     }
 }
