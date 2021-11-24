97212c8e2cfb8acf82fd51be847b9662c7d35715
Hoodz <laurens123987@gmail.com>
2021-11-24 13:12:17 +0100

    Update School.cs

--- Model/School.cs
+++ Model/School.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace Model
 {
-    class School
+    public class School
     {
+
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Study { get; set; }
+
+        public School(string name, string city, string study)
+        {
+            Name = name;
+            City = city;
+            Study = study;
+        }
+
     }
 }
