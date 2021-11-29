915c86f7ba541a8ebf51caee936a68a6a13be9c6
Hoodz <laurens123987@gmail.com>
2021-11-29 14:43:03 +0100

    profile update

--- Model/Profile.cs
+++ Model/Profile.cs
@@ -4,13 +4,30 @@ using System.Text;
 
 namespace Model
 {
-    class Profile
+    public class Profile
     {
 
-        //student
-        //qa
-        //morals
-        //interests
+        private Student Student { get; set; }
+        private QAData QAData { get; set; }
+        private MoralsData MoralsData { get; set; }
+        private InterestsData InterestsData { get; set; }
+        private string Description { get; set; }
+        private Media Media { get; set; }
+        private MatchList MatchList { get; set; }
+        private BlockList BlockList { get; set; }
+
+        public Profile(Student student)
+        {
+            Student = student;
+            QAData = new QAData(student);
+            MoralsData = new MoralsData(student);
+            InterestsData = new InterestsData(student);
+            Media = new Media(student);
+            MatchList = new MatchList(student);
+            BlockList = new BlockList(student);
+            Description = ""; //get description from db
+
+        }
 
     }
 }
