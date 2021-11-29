7544f4373ad86b429da2861a7e852592fdbc449c
Hoodz <laurens123987@gmail.com>
2021-11-29 21:09:46 +0100

    Update BlockList.cs

--- Model/BlockList.cs
+++ Model/BlockList.cs
@@ -4,12 +4,12 @@ using System.Text;
 
 namespace Model
 {
+    /*This class contains the list of all blocked students for the current user*/
     public class BlockList
     {
 
-        private List<Student> BlockedStudents { get; set; }
+        public List<Student> BlockedStudents { get; set; }
         private Student Student;
-
         public BlockList(Student student)
         {
             Student = student;
@@ -17,18 +17,21 @@ namespace Model
             //update list according to database
         }
 
+        /*Adds arg student to blocked list*/
         public void AddBlock(Student student)
         {
             BlockedStudents.Add(student);
             //database update
         }
 
+        /*removes arg student from blocked list*/
         public void RemoveBlock(Student student)
         {
             BlockedStudents.Remove(student);
             //database update
         }
 
+        /*returns if arg student is in the blocked list*/
         public bool IsBlocked(Student student)
         {
             return BlockedStudents.Contains(student);
