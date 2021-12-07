aeb52b6906104988844be0c34c68d4717e45022d
Bas Conrads <11506433+BastogneBas@users.noreply.github.com>
2021-12-07 14:42:31 +0100

    Added support for viewing pictures on profile page

---
+++ Gateway/MediaDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Gateway
+{
+    public static class MediaDataAccess
+    {
+        public static IEnumerable<string> GetUserMedia(int userID)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(FiddleHelper.GetConnectionStringSql("StudentMatcherDB")))
+            {
+                return connection.Query<string>("SELECT Path FROM UserMedia WHERE UserID = @uid", new { uid = userID });
+            }
+        }
+
+    }
+}
---
+++ ViewModel/ProfilePagePhotosViewModel.cs
@@ -0,0 +1,81 @@
+﻿using Gateway;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Input;
+using ViewModel.Commands;
+
+namespace ViewModel
+{
+    public class ProfilePagePhotosViewModel : INotifyPropertyChanged
+    {
+        public Uri SelectedImage
+        {
+            get
+            {
+                if (Images.Count > 0)
+                {
+                    if (_selectedImage > 0 && _selectedImage < Images.Count)
+                        return Images[_selectedImage];
+                    else
+                        return Images[_selectedImage % Images.Count];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        private int _selectedImage = 0;
+
+        public ObservableCollection<Uri> Images { get; } = new ObservableCollection<Uri>();
+
+        public ICommand PhotoNavigationButtonCommand => new RelayCommand(
+            //(parameter) =>
+            () =>
+            {
+                object parameter = "+"; // TODO: use given parameter when commit 63d05e29 is merged
+                if (Images.Count > 0)
+                {
+                    if ((string)parameter == "+")
+                    {
+                        _selectedImage++;
+                        _selectedImage %= Images.Count;
+                    }
+                    else if ((string)parameter == "-")
+                    {
+                        _selectedImage--;
+                        _selectedImage %= Images.Count;
+                    }
+                }
+                OnPropertyChanged("SelectedImage");
+            },
+            () => true
+            );
+
+        public ProfilePagePhotosViewModel()
+        {
+            List<string> imageStrings = new List<string>(MediaDataAccess.GetUserMedia(1));
+            Images = new ObservableCollection<Uri>(imageStrings.ConvertAll((str) => new Uri(str, UriKind.RelativeOrAbsolute)));
+        }
+
+        #region Property change notification
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Triggers the PropertyChanged event
+        /// </summary>
+        /// <param name="propertyName">The property which is changed</param>
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
