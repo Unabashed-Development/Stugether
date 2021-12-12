a13e5b436def2f69f766e20215b194ac4635a33c
Hoodz <laurens123987@gmail.com>
2021-12-12 20:30:55 +0100

    Update ProfileDataAccess.cs

--- Gateway/ProfileDataAccess.cs
+++ Gateway/ProfileDataAccess.cs
@@ -13,35 +13,70 @@ namespace Gateway
 
         public static List<Interest> LoadAllInterests()
         {
-            using IDbConnection connection = new System.Data.SqlClient.SqlConnection(FiddleHelper.GetConnectionStringSql("StudentMatcherDB"));
-            List<Interest> result = (List<Interest>)connection.Query<Interest>("SELECT * FROM InterestType"); //change to interests
-            return result;
+            try
+            {
+                using IDbConnection connection = new System.Data.SqlClient.SqlConnection(FiddleHelper.GetConnectionStringSql("StudentMatcherDB"));
+                string sql = "SELECT * FROM InterestType";
+                List<Interest> result = (List<Interest>)connection.Query<Interest>(sql);
+                return result;
+            } catch(Exception) 
+            {
+                return null;
+            }
         }
 
         public static InterestsData LoadInterestsData(int id)
         {
-            using IDbConnection connection = new System.Data.SqlClient.SqlConnection(FiddleHelper.GetConnectionStringSql("StudentMatcherDB"));
-            List<Interest> result = (List<Interest>)connection.Query<Interest>($"SELECT it.InterestID, it.InterestName, it.CategoryID FROM Interests i JOIN InterestType it ON it.InterestID = i.InterestID WHERE UserID = {id};");
-            return new InterestsData(id, result);
+            try
+            {
+                using IDbConnection connection = new System.Data.SqlClient.SqlConnection(FiddleHelper.GetConnectionStringSql("StudentMatcherDB"));
+                string sql = $"SELECT it.InterestID, it.InterestName, it.CategoryID FROM Interests i JOIN InterestType it ON it.InterestID = i.InterestID WHERE UserID = {id};";
+                List<Interest> result = (List<Interest>)connection.Query<Interest>(sql);
+                return new InterestsData(id, result);
+            }
+            catch (Exception) 
+            {
+                return null;
+            }
         }
 
         public static Profile LoadProfile(int id)
         {
-            using IDbConnection connection = new System.Data.SqlClient.SqlConnection(FiddleHelper.GetConnectionStringSql("StudentMatcherDB"));
-            Profile studentData = connection.QuerySingle<Profile>($"SELECT * FROM Profile WHERE UserID = {id};");
+            try
+            {
+                using IDbConnection connection = new System.Data.SqlClient.SqlConnection(FiddleHelper.GetConnectionStringSql("StudentMatcherDB"));
+                string sql = $"SELECT * FROM Profile WHERE UserID = {id};";
+                Profile studentData = connection.QuerySingle<Profile>(sql);
+                School school = LoadSchool(id);
+                studentData.School = school;
+                InterestsData interestData = LoadInterestsData(id);
+                studentData.InterestsData = interestData;
+                //QAData qaData = LoadQAData(id);
+                //studentData.QAData = qaData;
+                //MoralsData moralsData = LoadMoralsData(id);
+                //studentData.MoralsData = moralsData;
+                studentData.UserMedia = new List<Uri>(MediaDataAccess.GetUserMediaUris(id));
+                studentData.FirstUserMedia = studentData.UserMedia?.FirstOrDefault();
+                return studentData;
+            } catch (Exception) 
+            {
+                return null;
+            }
+        }
 
-            School school = LoadSchool(id);
-            studentData.School = school;
-            InterestsData interestData = LoadInterestsData(id);
-            studentData.InterestsData = interestData;
-            //QAData qaData = LoadQAData(id);
-            //studentData.QAData = qaData;
-            //MoralsData moralsData = LoadMoralsData(id);
-            //studentData.MoralsData = moralsData;
-            studentData.UserMedia = new List<Uri>(MediaDataAccess.GetUserMediaUris(id));
-            studentData.FirstUserMedia = studentData.UserMedia?.FirstOrDefault();
-
-            return studentData;
+        public static School LoadSchool(int id)
+        {
+            try
+            {
+                using IDbConnection connection = new System.Data.SqlClient.SqlConnection(FiddleHelper.GetConnectionStringSql("StudentMatcherDB"));
+                string sql = $"SELECT * FROM School WHERE UserID = {id};";
+                School schoolData = connection.QuerySingle<School>(sql);
+                return schoolData;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
         public static void CreateEmptyProfile(int id)
@@ -56,27 +91,18 @@ namespace Gateway
             UpdateSchool(newProfile.School);
         }
 
-        public static School LoadSchool(int id)
+        public static bool UpdateInterestsData(InterestsData interestsData)
         {
             try
             {
                 using IDbConnection connection = new System.Data.SqlClient.SqlConnection(FiddleHelper.GetConnectionStringSql("StudentMatcherDB"));
-                School schoolData = connection.QuerySingle<School>($"SELECT * FROM School WHERE UserID = {id};");
-                return schoolData;
-            }
-            catch (InvalidOperationException)
-            {
-                return null;
-            }
-        }
 
-        public static bool UpdateInterestsData(InterestsData interestsData)
-        {
-            using IDbConnection connection = new System.Data.SqlClient.SqlConnection(FiddleHelper.GetConnectionStringSql("StudentMatcherDB"));
-            try
-            {
                 //delete all existing interests
-                _ = connection.Execute($"DELETE FROM Interests WHERE UserID = {interestsData.UserID}");
+                string sqlDelete = "DELETE FROM Interests WHERE UserID = @UserID";
+                if (connection.Execute(sqlDelete, interestsData) == 0)
+                {
+                    return false;
+                }
 
                 //insert the new interests
                 StringBuilder sb = new StringBuilder();
@@ -86,44 +112,58 @@ namespace Gateway
                     return true;
                 }
 
+                //creates a string with all the values like: (UserID, InterestID), (UserId, InterestID)
                 interests.ForEach(interest =>
                 {
-                    sb.Append("(");
-                    sb.Append(interestsData.UserID);
-                    sb.Append(",");
-                    sb.Append(interest.InterestID);
-                    sb.Append(")");
-                    sb.Append(",");
+                    _ = sb.Append("(");
+                    _ = sb.Append(interestsData.UserID);
+                    _ = sb.Append(",");
+                    _ = sb.Append(interest.InterestID);
+                    _ = sb.Append(")");
+                    _ = sb.Append(",");
                 });
 
+                //remove the final , as its not necesarry
                 string values = sb.ToString().TrimEnd(',');
-                return connection.Execute($"Insert INTO Interests VALUES {values};") > 0;
+
+                //insert into the db
+                string sqlInsert = $"Insert INTO Interests VALUES {values};";
+                return connection.Execute(sqlInsert) > 0;
             }
-            catch (Exception) { }
-            return false;
+            catch (Exception) 
+            {
+                return false;
+            }
         }
 
         public static bool UpdateProfile(Profile profile)
         {
-            using IDbConnection connection = new System.Data.SqlClient.SqlConnection(FiddleHelper.GetConnectionStringSql("StudentMatcherDB"));
             try
             {
-                return connection.Execute($"UPDATE Profile SET Description = @Description, FirstName = @FirstName, LastName = @LastName, DateOfBirth = @DateOfBirth, City = @City, Sex = @Sex WHERE UserID = @UserID", profile) != 0 ||
-                       connection.Execute($"INSERT INTO Profile(UserID, Description, FirstName, LastName, DateOfBirth, City, Sex) VALUES (@UserID, @Description, @FirstName, @LastName, @DateOfBirth, @City, @Sex);", profile) > 0;
+                using IDbConnection connection = new System.Data.SqlClient.SqlConnection(FiddleHelper.GetConnectionStringSql("StudentMatcherDB"));
+                string sqlUpdate = "UPDATE Profile SET Description = @Description, FirstName = @FirstName, LastName = @LastName, DateOfBirth = @DateOfBirth, City = @City, Sex = @Sex WHERE UserID = @UserID";
+                string sqlInsert = "INSERT INTO Profile(UserID, Description, FirstName, LastName, DateOfBirth, City, Sex) VALUES (@UserID, @Description, @FirstName, @LastName, @DateOfBirth, @City, @Sex);";
+                return (connection.Execute(sqlUpdate, profile) > 0) || connection.Execute(sqlInsert, profile) > 0;
             }
-            catch (Exception) { }
-            return false;
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public static bool UpdateSchool(School school)
         {
-            using IDbConnection connection = new System.Data.SqlClient.SqlConnection(FiddleHelper.GetConnectionStringSql("StudentMatcherDB"));
             try
             {
-                return connection.Execute($"UPDATE School SET SchoolName = '{school.SchoolName}', SchoolCity = '{school.SchoolCity}', Study = '{school.Study}' WHERE UserID = {school.UserID};") != 0 || connection.Execute($"INSERT INTO School(UserID, SchoolName, SchoolCity, Study) VALUES ({school.UserID}, '{school.SchoolName}', '{school.SchoolCity}', '{school.Study}');") > 0;
+                using IDbConnection connection = new System.Data.SqlClient.SqlConnection(FiddleHelper.GetConnectionStringSql("StudentMatcherDB"));
+                string sqlUpdate = "UPDATE School SET SchoolName = @SchoolName, SchoolCity = @SchoolStudy, Study = @Study WHERE UserID = @UserID;";
+                string sqlInsert = "INSERT INTO School(UserID, SchoolName, SchoolCity, Study) VALUES (@UserID, @SchoolName, @SchoolCity, @Study);";
+                return (connection.Execute(sqlUpdate, school) > 0) || connection.Execute(sqlInsert, school) > 0;
             }
-            catch (Exception) { }
-            return false;
+            catch (Exception) 
+            {
+                return false;
+            }
         }
 
         public static QAData LoadQAData(int id)
