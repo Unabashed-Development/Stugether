11f16b35b7315285b34d85390278c8216dd1c4da
Thomas van de Visch <thomasvisch@outlook.com>
2021-12-13 15:46:35 +0100

    add search preference/ with save option

---
+++ Model/RelationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class RelationType
+    {
+        public bool Love { get; set; }
+        public bool Friend { get; set; }
+        public bool Business { get; set; }
+        public bool StudyBuddy { get; set; }
+    }
+}
