93791aa08abc4cfdde45270580f6c113527f270a
Hoodz <laurens123987@gmail.com>
2021-12-22 15:35:16 +0100

    Morals profile view + settings + tests

---
+++ Gateway.Test/Gateway_ProfileDataAccess_LoadMoralsData.cs
@@ -0,0 +1,33 @@
+﻿using Model;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gateway.Test
+{
+    class Gateway_ProfileDataAccess_LoadMoralsData
+    {
+
+        [Test]
+        public void CheckIfMoralsDataIsEmpty()
+        {
+            //data
+            MoralsData data = ProfileDataAccess.LoadMoralsData(-1);
+
+            //expected value is 0 (userID -1 doesnt exists)
+            Assert.IsEmpty(data.Morals);
+        }
+
+        [Test]
+        public void CheckIfInterestsIsNotEmpty()
+        {
+            //data
+            MoralsData data = ProfileDataAccess.LoadMoralsData(3);
+
+            //expected value is not null, User with id 3 has always a school
+            Assert.IsNotEmpty(data.Morals);
+        }
+
+    }
+}
---
+++ Model/Moral.cs
@@ -0,0 +1,29 @@
+﻿namespace Model
+{
+    public class Moral
+    {
+
+        #region properties
+        public int MoralID { get; set; }
+        public string MoralName { get; set; }
+        public int Percentage { get; set; }
+        #endregion
+
+        #region constructors
+        public Moral(int moralID, string moralName, int percentage)
+        {
+            MoralID = moralID;
+            MoralName = moralName;
+            Percentage = percentage;
+        }
+        #endregion
+
+        #region methods
+        public override string ToString()
+        {
+            return MoralName;
+        }
+        #endregion
+
+    }
+}
