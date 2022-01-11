6451f54882b92f48dfc3d2531f54fcde1f26c60b
Gitfoe <43130920+Gitfoe@users.noreply.github.com>
2022-01-11 15:28:59 +0100

    Workaround for #125
    When starting the application and the SSH connection doesn't work, an error page is shown.

---
+++ View/ServerError.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for ServerError.xaml
+    /// </summary>
+    public partial class ServerError : Page
+    {
+        public ServerError()
+        {
+            InitializeComponent();
+        }
+    }
+}
