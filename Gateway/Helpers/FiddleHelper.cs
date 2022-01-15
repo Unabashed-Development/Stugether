abbafa0fa7c28cbf25c188b0b62eafcad0255f89
Gitfoe <43130920+Gitfoe@users.noreply.github.com>
2022-01-15 12:38:55 +0100

    Can now open multiple instances of application (fixed #79)

--- Gateway/Helpers/FiddleHelper.cs
+++ Gateway/Helpers/FiddleHelper.cs
@@ -5,10 +5,15 @@ namespace Gateway
     public static class FiddleHelper
     {
         /// <summary>
-        /// Assigns the right connection name to the connection string.
+        /// Assigns the right connection name to the connection string using the randomized port.
         /// </summary>
         /// <param name="name">The name of the connection.</param>
         /// <returns>A string of the ConfigurationManager.</returns>
-        public static string GetConnectionStringSql(string name) => ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        public static string GetConnectionStringSql(string name)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings[name].ConnectionString;
+            int indexOfServer = connectionString.IndexOf(';');
+            return connectionString.Insert(indexOfServer, ',' + SSHService.SshPort.ToString());
+        }
     }
 }
